7ff1c7a9988f04a4d738585b12bf92d12d627994
Rondinelle Walitte <rondinellewalitte@gmail.com>
2022-03-02 13:54:50 -0300

    Update index.js
    enviar para email

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const config = {
     bodyFormData.append('media_final', '10');
     bodyFormData.append('iniciado', '2022-11-11+14:30:05');
     bodyFormData.append('concluido', '2022-02-10+14:30:05');
+    bodyFormData.append('envio', 'on');/* Enviar para email, remover essa linha */
 
     const { data } = await axios.post('https://saeecopuff.com.br/certificate/registerCertificate/adm/', bodyFormData, config);
     console.log(data.alert);
